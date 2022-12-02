2b111e10d6eb86bc6ae1c5f5c06b7720081d14a7
oddesa <jehnsen.hirena@sci.ui.ac.id>
2022-12-02 11:39:01 +0700

    Initial Commit

---
+++ Clone Netplifx/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Clone Netplifx
+//
+//  Created by Macbook on 02/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
